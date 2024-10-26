5a176a4494b776709c39072827e46d452a51ed55
Anurag9769 <singhanurag9769@gmail.com>
2024-10-26 12:59:43 +0530

    deleting unused code

--- Sample.py
+++
@@ -1,8 +0,0 @@
-from huggingface_hub import from_pretrained_fastai
-import pathlib
-
-temp = pathlib.PosixPath
-pathlib.PosixPath = pathlib.WindowsPath
-
-learn = from_pretrained_fastai("sacamiso/crops")
-print(learn.predict("cover-crops.png"))
